1e4253caa5d888cc97328f6b32b8482c4ce26e08
jzaragosa06 <junjunzaragosa2309@gmail.com>
2025-04-26 20:00:50 +0800

    refactor: remove endpoint to move to auth

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
+import { Controller } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {
-  constructor(private readonly userService: UsersService) {}
-
-  @Post()
-  create(@Body() dto: CreateUserDto) {
-    return this.userService.create(dto);
-  }
-}
+export class UsersController {}
